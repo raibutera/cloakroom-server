7a6c55a2e80335dc3baf070ab2daf97230a174a1
Rai Butera <rai@rbutera.com>
2014-09-26 12:55:08 +0100

    preparing for stryju's help

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
 var express = require('express');
 var app = express();
 var moment = require('moment');
+var modRewrite = require('connect-modrewrite');
 
 app.set('port', (process.env.PORT || 5000));
+
+
+var allowedExtensions = [
+  'html',
+  'js',
+  'map',
+  'css',
+  'png',
+  'svg',
+  'jpg',
+  'jpeg',
+  'gif',
+  'webp',
+  'woff',
+  'ttf',
+  'svg',
+  'otf',
+  'ico',
+  'eot',
+];
+
+var extensionsPattern = allowedExtensions
+  .map( function ( extension ) {
+    return '\\.' + extension;
+  })
+  .join( '|' );
+
+// ...
+
+app.use(modRewrite([
+  // '!' + extensionsPattern + '$ index.html [L]'
+  '!' + extensionsPattern + '$ index.html'
+]));
+
 app.use(express.static(__dirname + '/public'));
-
-app.use("/scripts", express.static(__dirname + "/public/scripts"));
-app.use("/images", express.static(__dirname + "/public/images"));
-app.use("/styles", express.static(__dirname + "/public/styles"));
-app.use("/views", express.static(__dirname + "/public/views"));
-app.use("/fonts", express.static(__dirname + "/public/fonts"));
-app.use("/vendor", express.static(__dirname + "/public/vendor"));
-
-// This route deals enables HTML5Mode by forwarding missing files to the index.html
-app.get('/[^\.]+$', function(req,res){
-    res.set('Content-Type', 'text/html')
-        .sendfile(__dirname + '/public/index.html');
-});
 
 app.listen(app.get('port'), function() {
   console.log("Cloakroom express server is running at localhost:" + app.get('port'));
